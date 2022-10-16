dd13b69cc82d7fe251a1e266370883d5a7002b25
gelevera <p.gelevera@cft.ru>
2022-10-16 15:32:59 +0700

    Добавил вёрстку

--- core/db_manager/views.py
+++ core/db_manager/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        # context={},
+    )

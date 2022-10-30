972439478af98e4a0cb9f1d997cb9332c1bdc467
gelevera <p.gelevera@cft.ru>
2022-10-30 18:19:48 +0700

    Закончил вёрстку

--- core/db_manager/urls.py
+++ core/db_manager/urls.py
@@ -3,6 +3,6 @@ from django.urls.conf import path, include
 from db_manager.views import Index
 
 urlpatterns = [
-    path('', Index.as_view()),
+    path('', Index.as_view(), name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]

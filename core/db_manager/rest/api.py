37625765528b736a20eb41315370f43dfed44a1e
Mark Beg <coollogin1234@gmail.com>
2022-12-14 20:35:13 +0600

    Доработан эндпоинт.

--- core/db_manager/rest/api.py
+++ core/db_manager/rest/api.py
@@ -6,14 +6,27 @@ from db_manager.models import Vehicle
 class VehicleSerializer(serializers.ModelSerializer):
     """Сериализатор данных для получения ограничений."""
     attributes = serializers.ReadOnlyField(source='get_hierarchy_attributes_json')
+    configurations = serializers.SerializerMethodField(read_only=True)
+
     # attributes = serializers.SerializerMethodField(read_only=True)
     #
     # def get_attributes(self, obj):
     #     return obj.get_hierarchy_attributes().to_json()
 
+    def get_configurations(self, obj: Vehicle) -> list[dict]:
+        """"""
+        configurations = Vehicle.objects.filter(parent=obj)
+        configurations_attrs = []
+        for config in configurations:
+            config_data = VehicleSerializer(config).data
+            config_data.pop('configurations')
+            configurations_attrs.append(config_data)
+
+        return configurations_attrs
+
     class Meta:
         model = Vehicle
-        exclude = ['attrs']
+        exclude = ['attrs', 'parent', '_type']
 
 
 class GetDataByNameRequest(serializers.Serializer):
